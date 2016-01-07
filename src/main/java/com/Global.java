e5a9635275a1c37cc2e6e1ed6db80983ce3bd7d9
AltrdGenetics <tofudeliveryae86@hotmail.com>
2016-01-07 09:53:18 -0500

    moved all of the files. Has the errors in there too

---
+++ src/main/java/com/Global.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Andrew
+ */
+public class Global {
+    
+    //INI Information
+    boolean mergeCapable    = false;
+    boolean newIndex        = false;
+    double RAM_BUFFER_SIZE  = 1024.0;
+    int dayOfWeek           = Calendar.FRIDAY;
+    int hourOfDay           = 20;
+    int MAX_BUFFERED_DOCS   = 10;
+    int MERGE_FACTOR        = 100;
+    int NUM_THREADS         = 1;
+    int THREAD_SLEEP        = 600000;
+    int WRITE_LIMIT         = 1000000000;
+    String dataDir          = null;
+    String indexDir         = null;
+    
+    //System Flags for Indexing
+    boolean executorRunning = false;
+    boolean exitNow         = false;
+    boolean forceExit       = false;
+    boolean lockIndex       = false;
+    boolean merge           = false;
+    Date lastIndexTime      = null;
+    
+    //Items for the Tray
+    boolean trayActive      = false;
+    SystemTray tray;
+    TrayIcon trayIcon;
+    
+    //Other Defaults
+    Thread threadOne, threadTwo;
+    MainPanel mainPanel;
+    SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss a");
+    
+    /**
+     * Links up the main panel for access across the application
+     * 
+     * @param mainPanel mainPanel class
+     */
+    public void setMainPanel(MainPanel mainPanel) {
+        this.mainPanel = mainPanel;
+    }
+
+    /**
+    * When building the list to index file this is the verified list of 
+    * types we can handle.
+    * 
+    * @param file The path for the file. We only look at the suffix of the path.
+    * @return boolean - (true/false) for verification of file type.
+    */
+    public static boolean validateFileType(Path file){
+        return  
+            //Microsoft Office Format
+            file.toString().endsWith(".wps")  ||
+            file.toString().endsWith(".doc")  ||
+            file.toString().endsWith(".docx") ||
+            file.toString().endsWith(".docm") ||
+            file.toString().endsWith(".ppt")  ||
+            file.toString().endsWith(".pptm") ||
+            file.toString().endsWith(".pptx") ||
+            file.toString().endsWith(".xlx")  ||
+            file.toString().endsWith(".xlxm") ||
+            file.toString().endsWith(".xlsx") ||
+
+            //Open Office Format
+            file.toString().endsWith(".odt")  ||
+            file.toString().endsWith(".odp")  ||
+            file.toString().endsWith(".ods")  ||
+
+            //Other
+            file.toString().endsWith(".pdf")  ||
+            file.toString().endsWith(".txt")  ||
+            file.toString().endsWith(".rtf")  ||
+            file.toString().endsWith(".epub") ||
+            file.toString().endsWith(".xml")  ||
+            file.toString().endsWith(".htm")  ||
+            file.toString().endsWith(".html") ||
+            file.toString().endsWith(".mhtml")||
+            file.toString().endsWith(".csv")   ;
+    }
+    
+    /**
+     * Pass the printout information to the main panel for display.
+     * 
+     * @param global This is for reference to the global class variables 
+     * and methods.
+     * @param text This is the information that will be displayed on the main
+     * panel.
+     */
+    public static void indexPanelPrintOut(Global global, String text) {
+        global.mainPanel.getjTextArea1().append(text);
+    }
+    
+}
---
+++ src/main/java/com/ThreadIndexing.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Andrew
+ */
+public class ThreadIndexing {
+    
+    private final boolean indexGUI = true;
+    
+    /**
+     * Thread for indexing with Lucene 
+     * 
+     * @param global This is for reference to the global class variables 
+     * and methods.
+     */
+    public void IndexThread(Global global) {
+        try {
+            Thread.sleep(1000);
+            while (true) {
+                try {
+                    //lock index so the application can cleanly exit.
+                    global.lockIndex = true;
+                    
+                    //Index the files to the database
+                    LuceneIndexer.IndexFiles(global, global.newIndex, indexGUI);
+                    
+                    //Printout the sleep information
+                    System.out.println("Sleeping for: " + TimeUnit.MILLISECONDS.toMinutes(global.THREAD_SLEEP) + "min");
+                    if (indexGUI){
+                        global.mainPanel.getjTextArea1().append("\n\n");
+                        global.mainPanel.getjTextArea1().append("Sleeping for: " + TimeUnit.MILLISECONDS.toMinutes(global.THREAD_SLEEP) + "min");
+                        global.mainPanel.getjTextArea1().append("\n\n\n\n");
+                    }
+                    //unlock the application
+                    global.lockIndex = false;
+                    
+                    //If exit flag exists exit the application safely
+                    if (global.exitNow == true){
+                        if (global.trayActive == true){
+                            global.tray.remove(global.trayIcon);
+                        }
+                        System.exit(0);
+                    }
+                    //Sleep the thread based on the INI file variable.
+                    Thread.sleep(global.THREAD_SLEEP);
+                    
+                } catch (InterruptedException ex) {
+                   Logger.getLogger(ThreadIndexing.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadIndexing.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
